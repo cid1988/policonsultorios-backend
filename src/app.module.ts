60703e72d69a1fc81ac9a0a07b79400c188a1991
Pablo <perellopablo1988@gmail.com>
2022-09-07 23:11:10 -0300

    Add sequelize, mariadb connection local

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,24 @@
 import { Module } from '@nestjs/common';
+import { SequelizeModule } from '@nestjs/sequelize';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    SequelizeModule.forRoot({
+      retryAttempts: 100,
+      dialect: 'mariadb',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: '1234',
+      database: 'policonsultorios',
+      models: [],
+      autoLoadModels: true,
+      synchronize: false,
+      logging: false
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
